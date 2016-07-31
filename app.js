a59723b75d3fbce1e7ecc852aa39de59f214911b
Ariel Tay <shihqian@gmail.com>
2016-08-01 01:26:35 +0800

    editing deployment stuff

--- app.js
+++ app.js
@@ -54,6 +54,6 @@ app.use((err, req, res, next) => {
   })
 })
 
-app.listen(process.env.PORT, () => {
+app.listen(process.env.PORT || 3000, () => {
   console.log(`Fantastic Server is listening on port ${process.env.PORT}`)
 })
